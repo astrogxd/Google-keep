5d4d1aacd5ca07feb491f04df93eb504118d315a
Amogelang Matlala <amatlala1510@gmail.com>
2024-02-10 10:05:43 +0200

    just adding js features

--- app.js
+++ app.js
@@ -5,14 +5,58 @@ class Note {
     this.text = text;
   }
 }
-
 class App {
   constructor() {
     this.notes = [];
+
+    this.$activeForm = document.querySelector(".active-form");
+    this.$inactiveForm = document.querySelector(".inactive-form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$notes = document.querySelector(".notes");
+
+    this.addEventListeners();
   }
-  addNote(id, { title, text }) {
-    const newNote = new Note(id, title, text);
-    this.notes = [...this.notes, newNote];
+
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+
+  handleFormClick(event) {
+    const isActiveFormClickedOn = this.$activeForm.contains(event.target);
+    const isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+
+    if (isInactiveFormClickedOn) {
+      this.openActiveForm();
+    } else if (!isInactiveFormClickedOn && !isActiveFormClickedOn) {
+      this.addNote({ title, text });
+      this.closeActiveForm();
+    }
+  }
+
+  openActiveForm() {
+    this.$inactiveForm.style.display = "none";
+    this.$activeForm.style.display = "block";
+    this.$noteText.focus();
+  }
+
+  closeActiveForm() {
+    this.$inactiveForm.style.display = "block";
+    this.$activeForm.style.display = "none";
+    this.$noteText.value = "";
+    this.$noteTitle.value = "";
+  }
+
+  addNote({ title, text }) {
+    if (text != "") {
+      const newNote = new Note(cuid(), title, text);
+      this.notes = [...this.notes, newNote];
+      this.displayNotes();
+    }
   }
 
   editNote(id, { title, text }) {
@@ -25,44 +69,65 @@ class App {
     });
   }
 
+  displayNotes() {
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) =>
+          `
+        <div class="note" id="${note.id}">
+        <span class="material-symbols-outlined check-circle">
+          check_circle
+        </span>
+        <div class="title">${note.title}</div>
+        <div class="text">${note.text}</div>
+        <div class="note-footer">
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >add_alert</span
+            >
+            <span class="tooltip-text">Remind me</span>
+          </div>
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >person_add</span
+            >
+            <span class="tooltip-text">Collaborator</span>
+          </div>
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >palette</span
+            >
+            <span class="tooltip-text">Background options</span>
+          </div>
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >image</span
+            >
+            <span class="tooltip-text">Add image</span>
+          </div>
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >archive</span
+            >
+            <span class="tooltip-text">Archive</span>
+          </div>
+          <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon"
+              >more_vert</span
+            >
+            <span class="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+
+        `
+      )
+      .join("");
+  }
+
   deleteNote(id) {
     this.notes = this.notes.filter((note) => note.id != id);
   }
-
-  displayNotes() {
-    this.notes = this.notes.map((note) =>
-      console.log(`
-    ID: ${note.id}
-    Title: ${note.title}
-    Text: ${note.text}`)
-    );
-  }
 }
 
-const note1 = {
-  title: "Test note",
-  text: "text",
-};
-
-const Updatednote = {
-  title: "updated Test note",
-  text: "updated text",
-};
 const app = new App();
-app.addNote(0, note1);
-app.addNote(1, note1);
-app.addNote(2, note1);
-app.addNote(3, note1);
-app.displayNotes();
-
-setTimeout(() => {
-  app.editNote(2, Updatednote);
-  app.displayNotes();
-}, 1000);
-
-setTimeout(() => {
-  app.deleteNote(2);
-  app.displayNotes();
-}, 2000);
-
-// learning JS dom by creating google keep
